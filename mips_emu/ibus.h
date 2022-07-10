2f001e8467b8746abca872a0a4a8e1a31c3658f3
Grzegorz Górnisiewicz <grzegorz.gornisiewicz@gmail.com>
2022-07-11 01:48:09 +0200

    update bus to use vector and to operate on bytes

--- mips_emu/ibus.h
+++ mips_emu/ibus.h
@@ -6,5 +6,5 @@ class IBus
 public:
 	virtual uint32_t Size() = 0;
 	virtual uint32_t Read(uint32_t addr) = 0;
-	virtual void Write(uint32_t addr, uint32_t data) = 0;
+	virtual void Write(uint32_t addr, uint8_t data) = 0;
 };
